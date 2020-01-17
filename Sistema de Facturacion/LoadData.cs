81719cef4589db6257c8382abb19749d07ea8fe9
Angel Lewis Del Rosario Terrero <20163553@itla.edu.do>
2020-01-17 15:37:43 -0400

    Complete Project.

---
+++ Sistema de Facturacion/LoadData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Facturacion
+{
+    class LoadData
+    {
+
+        public static int ID;
+        public static string Nombre;
+        public static string Email;
+        public static string Usuario;
+ 
+
+    }
+}
---
+++ Sistema de Facturacion/Proveedores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Facturacion
+{
+    class Proveedores
+    {
+
+        public int Id_Proveedor { get; set; }
+        public string Empresa { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string Representante { get; set; }
+        public string Direccion { get; set; }
+        public string Tipo_Proveedor { get; set; }
+
+
+
+    }
+}
